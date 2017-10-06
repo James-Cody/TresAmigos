a07d6acea760c5a5d5231f224e71dbea86b93278
Cody <Cody@Cody-I5>
2017-10-05 22:15:36 -0700

    10/05/17 Project Created

--- ThreeAmigos/src/ThreeAmigosModel/Scene.java
+++ ThreeAmigos/src/ThreeAmigosModel/Scene.java
@@ -6,6 +6,7 @@
 package ThreeAmigosModel;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 /**
  *
@@ -15,10 +16,43 @@ public class Scene implements Serializable{
     private String description;
     private double travelTime;
     private String blockedLocation;
-    private String displaySymbol;
-    
+    private String displaySymbol;   
+    private InventoryItem inventoryItems = new InventoryItem();
+    private ArrayList<Location> locations = new ArrayList<Location>();
     
     public Scene(){
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getTravelTime() {
+        return travelTime;
+    }
+
+    public void setTravelTime(double travelTime) {
+        this.travelTime = travelTime;
+    }
+
+    public String getBlockedLocation() {
+        return blockedLocation;
+    }
+
+    public void setBlockedLocation(String blockedLocation) {
+        this.blockedLocation = blockedLocation;
+    }
+
+    public String getDisplaySymbol() {
+        return displaySymbol;
+    }
+
+    public void setDisplaySymbol(String displaySymbol) {
+        this.displaySymbol = displaySymbol;
     }
     
 }
