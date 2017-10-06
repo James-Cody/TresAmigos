d3a708192488defb6b8fe4f28930ff593b5a1cbe
Cody <Cody@Cody-I5>
2017-10-05 21:57:30 -0700

    10/05/17 Project Created

---
+++ ThreeAmigos/src/ThreeAmigosModel/Scene.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ThreeAmigosModel;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Cody
+ */
+public class Scene implements Serializable{
+    private String description;
+    private double travelTime;
+    private String blockedLocation;
+    private String displaySymbol;
+    
+    
+    public Scene(){
+    }
+    
+}
