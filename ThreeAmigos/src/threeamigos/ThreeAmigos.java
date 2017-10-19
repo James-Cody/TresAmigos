15324dc0f34e46f44e5f518434ce52e93dcac721
user <user@user-PC>
2017-10-19 00:08:17 -0500

    18/10/2017 By Govert Carreño

--- ThreeAmigos/src/threeamigos/ThreeAmigos.java
+++ ThreeAmigos/src/threeamigos/ThreeAmigos.java
@@ -5,12 +5,6 @@
  */
 package threeamigos;
 
-import ThreeAmigosModel.City;
-import ThreeAmigosModel.Progress;
-import ThreeAmigosModel.Question;
-import ThreeAmigosModel.Scenes;
-import ThreeAmigosModel.TravelingCondition;
-import java.sql.Date;
 
 /**
  *
@@ -23,38 +17,6 @@ public class ThreeAmigos {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        City city = new City();
-        Progress progress = new Progress();
-        Question question = new Question();
-        TravelingCondition travelingCondition = new TravelingCondition();
-        Scenes scene = new Scenes();
-        
-        
-        city.setName("Example Name");
-        city.setDescription("Example of description");
-        
-        progress.setCitiesVisited(5);        
-        int[][] loc = new int[][]{{1,0},
-                                  {0,0},
-                                  {0,0},
-                                  {0,0}};
-                
-        progress.setLocation(loc);
-        progress.setTotalTime(123456789);
-        
-        question.setQuestion("Quiestion example");
-        question.setAnswer("Answer Example");
-        
-        travelingCondition.setType("Animal Atack (Example)");
-        travelingCondition.setEffect(3);
-        
-        scene.setDescription("Description example");
-        
-        System.out.println(city.toString());
-        System.out.println(progress.toString());
-        System.out.println(question.toString());
-        System.out.println(travelingCondition.toString());
-        System.out.println(scene.toString());
         
     }
     
